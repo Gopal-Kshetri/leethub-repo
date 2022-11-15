76f74d67094cb8c5186ae9fb8828753b2bf42353
Soy <075bct039.gopal@pcampus.edu.np>
2022-11-15 15:44:28 +0545

    Time: 170 ms (35.18%), Space: 42.1 MB (84.35%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // let result = [];
+    for(var i=0; i<nums.length; i++){
+        for(var j=i+1; j<nums.length; j++){
+            if((nums[i]+nums[j])==target)
+            {
+                // result.push(i,j);
+                return([i,j])
+            }
+        }
+    }
+    // return(result);
+};
+
