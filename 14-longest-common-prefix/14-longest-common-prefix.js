9a17073b78b507e58e7930938b9e0fb067732939
Soy <075bct039.gopal@pcampus.edu.np>
2022-11-20 09:05:50 +0545

    Time: 114 ms (29.85%), Space: 44 MB (21.63%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -8,17 +8,13 @@ var longestCommonPrefix = function(strs) {
     return "";
   for (i = 1; i <= max; i++) {
     text = strs[0].substring(0, i)
-    // console.log("text:",text);
     for (j = 0; j < strs.length; j++) {
-      // console.log('check strings');
       if (text != strs[j].substring(0, i)) {
-        // console.log('false');
         prefix = false;
       }
     }
     if (prefix) {
       longest = text;
-      console.log("longest:", longest);
     }
   }
   return longest;
