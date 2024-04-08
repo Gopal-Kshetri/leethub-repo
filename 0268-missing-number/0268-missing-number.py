f8a8edae4227877cc2a91ab0886d2d89439fb51b
Soy <075bct039.gopal@pcampus.edu.np>
2024-04-08 16:10:16 +0545

    Time: 110 ms (65.55%), Space: 17.7 MB (76.63%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        max_num = max(nums)
-        # min_num = min(nums)
-        for i in range(max(max_num, len(nums))+1):
-            if i not in nums:
+        n = len(nums)
+        v = [-1] * (n+1)
+        for num in nums:
+            v[num] = num
+        for i in range(len(v)):
+            if v[i] == -1:
                 return i
+        return 0
